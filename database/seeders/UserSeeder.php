a502028a191e0ba97f8300c8a9003d8a5a6e33ee
Daniel Buxton <dan.buxton99@gmail.com>
2022-09-15 22:02:27 +0100

    Initial Commit
    Added migration and seed files

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory(3)->create();
+
+        \Illuminate\Support\Facades\DB::table('module_user')
+            ->insertOrIgnore([
+                [
+                    'module_id' => 'COSE50637',
+                    'user_id' => 1,
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s')
+                ],
+                [
+                    'module_id' => 'COSE50586',
+                    'user_id' => 1,
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s')
+                ],
+                [
+                    'module_id' => 'COMP50017',
+                    'user_id' => 1,
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s')
+                ],
+                [
+                    'module_id' => 'COMP50016',
+                    'user_id' => 1,
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s')
+                ],
+                [
+                    'module_id' => 'COSE50637',
+                    'user_id' => 2,
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s')
+                ],
+                [
+                    'module_id' => 'COSE50586',
+                    'user_id' => 2,
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s')
+                ],
+                [
+                    'module_id' => 'COSE50582',
+                    'user_id' => 2,
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s')
+                ],
+                [
+                    'module_id' => 'COMP50016',
+                    'user_id' => 2,
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s')
+                ],
+                [
+                    'module_id' => 'COSE50637',
+                    'user_id' => 3,
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s')
+                ],
+                [
+                    'module_id' => 'COSE50586',
+                    'user_id' => 3,
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s')
+                ],
+                [
+                    'module_id' => 'COMP50016',
+                    'user_id' => 3,
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s')
+                ],
+                [
+                    'module_id' => 'COIS51092',
+                    'user_id' => 3,
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s')
+                ],
+                [
+                    'module_id' => 'COIS51091',
+                    'user_id' => 3,
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s')
+                ]
+            ]);
+    }
+}
